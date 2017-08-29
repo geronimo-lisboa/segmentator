453debc73553eacb6fea61c1c13b6c8e679526f7
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-08-28 21:04:43 -0300

    Voltando a funcionar com as minhas classes.

--- myVolume.cpp
+++ myVolume.cpp
@@ -19,26 +19,33 @@ MyVolume::MyVolume(itk::Image<float, 3>::Pointer ptr)
 	importToVTK->SetDataOrigin(oX, oY, oZ);
 	importToVTK->SetWholeExtent(0, szX - 1, 0, szY - 1, 0, szZ - 1);
 	importToVTK->SetDataExtentToWholeExtent();
-	importToVTK->SetDataScalarTypeToShort();
+	importToVTK->SetDataScalarTypeToFloat();
 	void* inputImagePtr = inputImage->GetBufferPointer();
 	importToVTK->SetImportVoidPointer(inputImagePtr, 1);
 	importToVTK->Update();
 	//Agora o mapper
 	mapper = vtkSmartPointer<vtkGPUVolumeRayCastMapper>::New();
 	mapper->SetMaxMemoryFraction(0.9);
+	mapper->GetLockSampleDistanceToInputSpacingMinValue();
 	mapper->SetInputData((vtkImageData*)importToVTK->GetOutputDataObject(0));
 	mapper->Update();
 	properties = vtkSmartPointer<vtkVolumeProperty>::New();
-	//opacityFunction = vtkSmartPointer<vtkPiecewiseFunction>::New();
-	//opacityFunction->AddSegment(0, 0, 300, 0.2);
-	//opacityFunction->AddSegment(301, 0.2, 800, 0.8);
-	//opacityFunction->AddSegment(801, 0.8, 1200, 1);
-	//properties->SetScalarOpacity(opacityFunction);
-	//colorFunction = vtkSmartPointer<vtkColorTransferFunction>::New();
-	//colorFunction->AddRGBSegment(0, 0, 0, 0, 300, 1, 0, 0);
-	//colorFunction->AddRGBSegment(301, 1, 0, 0, 800, 1, 1, 0);
-	//colorFunction->AddRGBSegment(801, 1, 1, 0, 1200, 1, 1, 1);
-	//properties->SetColor(colorFunction);
+	opacityFunction = vtkSmartPointer<vtkPiecewiseFunction>::New();
+	opacityFunction->AddSegment(0.0, 0.0,    1000.0, 0.0);
+	opacityFunction->AddSegment(1000.0, 0.0, 1400.0, 0.2);
+	opacityFunction->AddSegment(1400.0, 0.2, 2000.0, 0.8);
+	opacityFunction->AddSegment(2000.0, 0.8, 3000.0, 1.0);
+
+	opacityFunction->ClampingOn();
+	properties->SetScalarOpacity(opacityFunction);
+	colorFunction = vtkSmartPointer<vtkColorTransferFunction>::New();
+	colorFunction->AddRGBSegment(0.0, 0.0,0.0,0.0, 1000.0, 0.0,0.0,0.0);
+	colorFunction->AddRGBSegment(1000.0, 1.0, 0.0, 0.0, 1400.0, 1.0, 0.0, 0.0);
+	colorFunction->AddRGBSegment(1400.0, 1.0, 1.0, 0.0, 2000.0, 1.0, 1.0, 0.0);
+	colorFunction->AddRGBSegment(2000.0, 1.0, 1.0, 1.0, 3000.0, 1.0, 1.0, 1.0);
+
+	colorFunction->ClampingOn();
+	properties->SetColor(colorFunction);
 	properties->ShadeOn();
 	//Propriedades definindas
 	actor = vtkSmartPointer<vtkVolume>::New();
--- segmentator.cpp
+++ segmentator.cpp
@@ -8,81 +8,20 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	//ImageLoader imgLoader;
-	//itk::Image<float, 3>::Pointer imagem = imgLoader.LoadImage("C:\\dicom\\Marching Man");
-	//vtkSmartPointer<vtkImageImport> importToVTK = vtkSmartPointer<vtkImageImport>::New();
-	//int szX = imagem->GetLargestPossibleRegion().GetSize()[0];
-	//int szY = imagem->GetLargestPossibleRegion().GetSize()[1];
-	//int szZ = imagem->GetLargestPossibleRegion().GetSize()[2];
-	//double sX = imagem->GetSpacing()[0];
-	//double sY = imagem->GetSpacing()[1];
-	//double sZ = imagem->GetSpacing()[2];
-	//double oX = imagem->GetOrigin()[0];
-	//double oY = imagem->GetOrigin()[1];
-	//double oZ = imagem->GetOrigin()[2];
-	//importToVTK->SetDataSpacing(sX, sY, sZ);
-	//importToVTK->SetDataOrigin(oX, oY, oZ);
-	//importToVTK->SetWholeExtent(0, szX - 1, 0, szY - 1, 0, szZ - 1);
-	//importToVTK->SetDataExtentToWholeExtent();
-	//importToVTK->SetDataScalarTypeToFloat();
-	//void* inputImagePtr = imagem->GetBufferPointer();
-	//importToVTK->SetImportVoidPointer(inputImagePtr, 1);
-	//importToVTK->Update();
-	vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
-	reader->SetDirectoryName("C:\\dicom\\Marching Man");
-	reader->Update();
-
-	vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
-	vtkSmartPointer<vtkRenderer> ren1 = vtkSmartPointer<vtkRenderer>::New();
-	ren1->SetBackground(0.1, 0.4, 0.2);
-	renWin->AddRenderer(ren1);
-	renWin->SetSize(301, 300); // intentional odd and NPOT  width/height
-	vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	iren->SetRenderWindow(renWin);
-	renWin->Render(); // make sure we have an OpenGL context.
-	vtkSmartPointer<vtkSmartVolumeMapper> volumeMapper = vtkSmartPointer<vtkSmartVolumeMapper>::New();
-	volumeMapper->SetBlendModeToComposite(); // composite first
-	volumeMapper->SetInputConnection(reader->GetOutputPort());
-	vtkSmartPointer<vtkVolumeProperty> volumeProperty = vtkSmartPointer<vtkVolumeProperty>::New();
-	volumeProperty->ShadeOff();
-	volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
-	vtkSmartPointer<vtkPiecewiseFunction> compositeOpacity = vtkSmartPointer<vtkPiecewiseFunction>::New();
-	compositeOpacity->AddPoint(0.0, 0.0);
-	compositeOpacity->AddPoint(900.0, 0.0);
-	compositeOpacity->AddPoint(1500.0, 1.0);
-	compositeOpacity->AddPoint(3000.0, 0.2);
-	compositeOpacity->ClampingOn();
-	volumeProperty->SetScalarOpacity(compositeOpacity); // composite first.
-
-	vtkSmartPointer<vtkColorTransferFunction> color = vtkSmartPointer<vtkColorTransferFunction>::New();
-	color->AddRGBPoint(0.0, 0.0, 0.0, 1.0);
-	color->AddRGBPoint(40.0, 1.0, 0.0, 0.0);
-	color->AddRGBPoint(255.0, 1.0, 1.0, 1.0);
-	color->ClampingOn();
-	volumeProperty->SetColor(color);
-
-	vtkSmartPointer<vtkVolume> volume = vtkSmartPointer<vtkVolume>::New();
-	volume->SetMapper(volumeMapper);
-	volume->SetProperty(volumeProperty);
-	ren1->AddViewProp(volume);
-	ren1->ResetCamera();
-
-	renWin->Render();
-	iren->Start();
 	////Criação das telas
-	//shared_ptr<Tela> tela00, tela01;
-	//tela00 = make_shared<Tela>();
-	//tela00->Render();
-	//tela01 = make_shared<Tela>();
-	//tela01->CriarObjetoDeTeste();
-	//tela01->Render();
-	////Carga da imagem e po-la na tela
-	//ImageLoader imgLoader;
-	//itk::Image<float, 3>::Pointer imagem = imgLoader.LoadImage("C:\\dicom\\Marching Man");
-	//MyVolume volume(imagem);
-	//tela00->AddVolume(volume);
-	////Começa o loop de eventos
-	//tela01->StartEventLoop();
-	//return EXIT_SUCCESS;
+	shared_ptr<Tela> tela00, tela01;
+	tela00 = make_shared<Tela>();
+	tela00->Render();
+	tela01 = make_shared<Tela>();
+	tela01->CriarObjetoDeTeste();
+	tela01->Render();
+	//Carga da imagem e po-la na tela
+	ImageLoader imgLoader;
+	itk::Image<float, 3>::Pointer imagem = imgLoader.LoadImage("C:\\dicom\\Marching Man");
+	MyVolume volume(imagem);
+	tela00->AddVolume(volume);
+	//Começa o loop de eventos
+	tela01->StartEventLoop();
+	return EXIT_SUCCESS;
 }
 
